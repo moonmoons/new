44e6f98c8ae05bc6a2674e7696172982442265ec
moonmoons <105143394+moonmoons@users.noreply.github.com>
2025-05-09 19:50:56 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import './App.css';
 const App = () => {
   // 더미 게시글 데이터
   const posts = [
-    { id: 1, title: '첫 번째 게시글', content: '이것은 첫 번째 게시글 내용입니다.' },
-    { id: 2, title: '두 번째 게시글', content: '두 번째 게시글 내용입니다.' },
-    { id: 3, title: '세 번째 게시글', content: '세 번째 게시글 내용입니다.' },
+    { id: 1, title: '리액트', content: '화면출력 공부 입니다.' },
+    { id: 2, title: '첫 번째 게시글', content: '이것은 첫 번째 게시글 내용입니다.' },
+    { id: 3, title: '두 번째 게시글', content: '두 번째 게시글 내용입니다.' },
+    { id: 4, title: '세 번째 게시글', content: '세 번째 게시글 내용입니다.' },
   ];
 
   return (
