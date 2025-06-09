4df657674f759bcfb5e5f949c69c11d3bc774b9d
moonmoons <105143394+moonmoons@users.noreply.github.com>
2025-06-09 13:48:31 +0900

    header 정도 버튼 할성화 추가

--- script/common.js
+++ script/common.js
@@ -8,3 +8,31 @@ $(function () {
       $("#main-content").load(page);
     });
   });
+
+
+function page_change(a){
+
+    $('.nav-button').removeClass('active');
+    $('#'+event.target.id).addClass('active');
+    /*$('.grid-container').toggleClass('hide-sidebar');*/
+    if('nav_home' == event.target.id ){
+        window.location.href= './index.html';
+        $('.grid-container').removeClass('hide-sidebar');
+        $('#'+event.target.id).addClass('hide-sidebar');
+    }
+    else if('nav_portfolio' == event.target.id ){
+        $('.grid-container').removeClass('hide-sidebar');
+
+        $("#sidebar").load("./layout/sidebar_portfolid.html");
+        $("#main-content").load("./board.html");
+    }
+    else if('nav_history' == event.target.id ){
+        $('.grid-container').removeClass('hide-sidebar');
+
+        $("#sidebar").load("./layout/sidebar.html");
+        $("#main-content").load("./history/page_main.html");
+    }
+}
+
+
+
